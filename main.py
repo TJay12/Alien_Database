63cd6181a38c0968bcaa2f4607b7dd8f6abe5632
PixelLover <41162341+TJay12@users.noreply.github.com>
2025-05-21 14:34:07 +1200

    Search Database Commit

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from Data.aliens import alien_database
 from seen_aliens import seen_all
+from alien_search import *
 
+# For 'Search Database' dev purpose
 seen_all()
 
-# View all seen aliens
-print("Seen alien races:")
-for alien in alien_database.values():
-    if alien.seen:
-        print(alien)
+def search_database():
+    search_by = input("Search By: ((V)iew all, (P)lanet, (A)ttitude)").lower()
+    if search_by == "v":
+        view_all()
+    elif search_by == "p":
+        planet_search()
+    elif search_by == "a":
+        attitude_search()
+    else:
+        print("Invalid Option")
+
+search_database()
