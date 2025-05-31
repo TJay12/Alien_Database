f6afe9f90c8d4eba2b2462770a0d0cc983a10a94
PixelLover <41162341+TJay12@users.noreply.github.com>
2025-05-31 15:25:52 +1200

    Restructure database and search Commit

--- alien_search.py
+++ alien_search.py
@@ -1,42 +1,87 @@
-# View all seen aliens
-def view_all():
-    print("Seen alien races:")
-    for alien in alien_database.values():
-        if alien.seen:
-            print(alien)
+from JSON_util import read_json
+from pathlib import Path
 
+root = Path("data")
+file = Path("database.json")
+path = root / file
+
+
+# <--- Search all Items in a Specific Category --->
 # Search by Planet
 def planet_search():
-    planets = []
-    for race in alien_database.values():
-        add_planet = race.planet
-        if add_planet not in planets:
-            planets.append(add_planet)
-    for planet in planets:
-        print(f" - {planet}")
-
+    data, planets = view_all_planets()
     planet = input("Enter Planet: ")
     if planet not in planets:
-        print("Planet not found")
+        print(f"{planet} not found")
     else:
         print(f"Alien Races on Planet {planet}:")
-        for race in alien_database:
-            if alien_database[race].planet == planet:
-                attitude = alien_database[race].friendliness
-                print(f" - {race} ({attitude})")
+        for race, info in data[planet].items():
+            attitude = data[planet][race]["attitude"]
+            print(f" - {race} ({attitude})")
 
+# Search by Races
+def race_search():
+    data, races = view_all_races()
+
+    input_race = input("Enter Race: ")
+    if input_race not in races:
+        print(f"{input_race} not found")
+    else:
+        for planet, race in data.items():
+            for race, info in data[planet].items():
+                if input_race == race:
+                    attitude = data[planet][race]["attitude"]
+        print(f"{input_race} is from {planet} ({attitude})")
+
+# Search by Attitude
 def attitude_search():
-    attitudes = []
-    for race in alien_database.values():
-        add_attitude = race.friendliness
-        if add_attitude not in attitudes:
-            attitudes.append(add_attitude)
-    for attitude in attitudes:
-        print(f"{attitude}, ",end="")
+    data, attitudes = view_all_attitudes()
 
-    attitude = input("\nEnter Attitude: ")
-    print(f"{attitude} Alien Races:")
-    for race in alien_database:
-        if alien_database[race].friendliness == attitude:
-            planet = alien_database[race].planet
-            print(f" - {race} ({planet})")
+    atude_imput = input("\nEnter Attitude: ")
+    print(f"{atude_imput} Alien Races:")
+    for planet, race in data.items():
+        for race, attitude in data[planet].items():
+            attitude = data[planet][race]["attitude"]
+            if attitude == atude_imput:
+                print(f" - {race} ({planet})")
+
+# <--- View all Items of a Specific Category --->
+def view_all_planets():
+    data = read_json(path)
+    planets = []
+    for planet in data:
+        if planet not in planets:
+            planets.append(planet)
+    for planet in planets:
+        print(f" - {planet}")
+    return data, planets
+
+def view_all_races():
+    data = read_json(path)
+    races = []
+    for planet, race in data.items():
+        for race in data[planet]:
+            if race not in races:
+                races.append(race)
+    for race in races:
+        print(f" - {race}")
+    return data, races
+
+def view_all_attitudes():
+    data = read_json(path)
+    attitudes = []
+    for planet, race in data.items():
+        for race, attitude in data[planet].items():
+            attitude = data[planet][race]["attitude"]
+            if attitude not in attitudes:
+                attitudes.append(attitude)
+    for attitude in attitudes:
+        print(f" - {attitude}")
+    return data, attitudes
+
+# view_all_planets()
+# view_all_races()
+# view_all_attitudes()
+planet_search()
+# race_search()
+# attitude_search()
--- dev_util.py
+++ dev_util.py
@@ -1,3 +1,6 @@
+import json
+from xml.etree.ElementTree import indent
+
 import JSON_util as j
 from pathlib import Path
 
@@ -6,18 +9,43 @@ file = Path("database.json")
 path = root / file
 
 def add_to_database():
+    planet = input("Planet: ")
     race = input("Race: ")
     friendliness = input("Attitude: ")
-    planet = input("Planet: ")
 
     data = j.read_json(path)
-
-    data[race] = {"race": race, "attitude": friendliness, "planet": planet}
+    if planet not in data:
+        data[planet] = {}
+        data[planet][race] = {"attitude": friendliness}
+    elif race in data[planet]:
+        print("Race already recorded on this planet")
+    else:
+        data[planet][race] = {"attitude": friendliness}
+        print(f"{race} from {planet} added to database")
 
     j.save_json(path, data)
-    print(f"{race} from {planet} added to database")
+
+def refactor_dict():
+    data = j.read_json(path)
+    newdata = {}
+
+    planets = []
+    for race in data:
+        planet = data[race]["planet"]
+        if planet not in planets:
+            planets.append(planet)
+
+    for planet in planets:
+        for race in data:
+            if data[race]["planet"] == planet:
+                if planet not in newdata:
+                    newdata[planet] = {}
+                    newdata[planet][race] = {"attitude": data[race]["attitude"]}
+                else:
+                    newdata[planet][race] = {"attitude": data[race]["attitude"]}
+
+    j.save_json(path, newdata)
 
 entries = int(input("New entries: "))
 for i in range(entries):
     add_to_database()
-
